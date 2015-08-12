0d25e6f4a1fcaf1e4a779fb5a9555f4617b0cf82
Ramon Servadei <ramon.servadei@gmail.com>
2015-08-12 23:31:30 +0100

    Revert "Use ConcurrentHashMap for holding ProxyContextPublishers"
    This reverts commit 64f55a43cd0b2392b713cb25cd6356abe952a8eb.

--- src/main/java/com/fimtra/datafission/core/Publisher.java
+++ src/main/java/com/fimtra/datafission/core/Publisher.java
@@ -16,9 +16,11 @@
 package com.fimtra.datafission.core;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ScheduledFuture;
@@ -321,18 +323,19 @@ public class Publisher
                 Publisher.this.connectionsRecord.getOrCreateSubMap(getTransmissionStatisticsFieldName(client));
             final EndPointAddress endPointAddress = Publisher.this.server.getEndPointAddress();
             final String clientSocket = client.getEndPointDescription();
-            submapConnections.put(IContextConnectionsRecordFields.PUBLISHER_ID,
-                new TextValue(Publisher.this.context.getName()));
-            submapConnections.put(IContextConnectionsRecordFields.PUBLISHER_NODE,
-                new TextValue(endPointAddress.getNode()));
+            submapConnections.put(IContextConnectionsRecordFields.PUBLISHER_ID, new TextValue(
+                Publisher.this.context.getName()));
+            submapConnections.put(IContextConnectionsRecordFields.PUBLISHER_NODE, new TextValue(
+                endPointAddress.getNode()));
             submapConnections.put(IContextConnectionsRecordFields.PUBLISHER_PORT,
                 LongValue.valueOf(endPointAddress.getPort()));
-            submapConnections.put(IContextConnectionsRecordFields.PROXY_ENDPOINT, new TextValue(clientSocket));
+            submapConnections.put(IContextConnectionsRecordFields.PROXY_ENDPOINT, new TextValue(
+                clientSocket));
             submapConnections.put(IContextConnectionsRecordFields.PROTOCOL, new TextValue(
                 getProxyContextPublisher(client).codec.getClass().getSimpleName()));
-
+            
             scheduleStatsUpdateTask();
-
+            
             Log.log(this, "Constructed for ", ObjectUtils.safeToString(client));
         }
 
@@ -515,7 +518,8 @@ public class Publisher
         super();
         this.context = context;
         this.lock = new ReentrantLock();
-        this.proxyContextPublishers = new ConcurrentHashMap<ITransportChannel, Publisher.ProxyContextPublisher>();
+        this.proxyContextPublishers =
+            Collections.synchronizedMap(new HashMap<ITransportChannel, Publisher.ProxyContextPublisher>());
         this.connectionsRecord = Context.getRecordInternal(this.context, ISystemRecordNames.CONTEXT_CONNECTIONS);
 
         // prepare to periodically publish status changes
@@ -652,6 +656,7 @@ public class Publisher
                 }, this.contextConnectionsRecordPublishPeriodMillis, this.contextConnectionsRecordPublishPeriodMillis,
                 TimeUnit.MILLISECONDS);
 
+        // todo wrong synchronization...
         // reschedule the stats update tasks at the new period
         for (ProxyContextPublisher proxyContextPublisher : this.proxyContextPublishers.values())
         {
@@ -782,14 +787,18 @@ public class Publisher
 
     ProxyContextPublisher getProxyContextPublisher(ITransportChannel client)
     {
-        final ProxyContextPublisher proxyContextPublisher = this.proxyContextPublishers.get(client);
-        if (proxyContextPublisher == null)
+        // todo can this be replaced with a ConcurrentHashMap?
+        synchronized (this.proxyContextPublishers)
         {
-            // ProxyContextPublisher only constructed on channel connection!
-            throw new NullPointerException("No ProxyContextPublisher for " + ObjectUtils.safeToString(client)
-                + ", is the channel closed?");
+            final ProxyContextPublisher proxyContextPublisher = this.proxyContextPublishers.get(client);
+            if (proxyContextPublisher == null)
+            {
+                // ProxyContextPublisher only constructed on channel connection!
+                throw new NullPointerException("No ProxyContextPublisher for " + ObjectUtils.safeToString(client)
+                    + ", is the channel closed?");
+            }
+            return proxyContextPublisher;
         }
-        return proxyContextPublisher;
     }
 
     /**
